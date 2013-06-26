a7ee0e36c32de18515ec40aee1bd3ad27992025a
Jan Vogt <news.jan.vogt@me.com>
2013-06-26 02:47:30 +0200

    Complete (almost) model description

--- Rscripts/chunks.R
+++ Rscripts/chunks.R
@@ -1,7 +1,10 @@
-## @knitr dataSetup
-rec.data <- read.csv(file="data/Rec.csv", sep=";") 
+## @knitr Demographics
+demographics.df <- read.csv(file="data/Demographics.csv", sep=";")
+
+## @knitr DataSetup
+rec.data <- read.csv(file="data/Rec.csv", sep=";")
 dists.list <- emp.dists(rec.data)
-fit.data <- do.fitting(dists.list, EVSDT, MPT1HTM2g, UVSDT, MPT2HTM)
+#fit.data <- do.fitting(dists.list, EVSDT, MPT1HTM2g, UVSDT, MPT2HTM)
 dists.list <- fit.data$data
 model.param <- fit.data$fit.par
 model.comp <- fit.data$model.comp
@@ -9,4 +12,4 @@ gsq <- exctractModelCriteria(model.param)
 ggplot(data=gsq[order(gsq[, "group"],gsq[, "session"],gsq[, "EVSDT_G.Squared"]-gsq[, "MPT1HTM2g_G.Squared"]),], aes(x=seq_along(code), y=EVSDT_G.Squared-MPT1HTM2g_G.Squared, group=paste(group, session), colour=factor(paste("group:", ifelse(group==1, "'fully encoded',", "'forced guessing',"), "session:", session))))+geom_line()+geom_point()+scale_y_continuous(expression(G[EVSDT]^2 - G[MPT1HTM2g]^2))+scale_x_continuous(expression(participant %.% session))+geom_hline(aes(yintercept=0))+scale_color_discrete("", guide=guide_legend(nrow=2))+theme(legend.position="top")
 
 ## @knitr AllPlots
-plot.vps(dists.list, model.param)
+#plot.vps(dists.list, model.param)
